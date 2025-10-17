8075afe0ef3ee0da6f6ef4ef590cba68e67edf91
Tiberius4114 <mzcloner7@gmail.com>
2025-10-17 21:54:21 +0330

    change gitignore and other stuff

--- express-server.js
+++ express-server.js
@@ -1,15 +1,13 @@
-const http = require("http")
-const express = require("express")
+const http = require("http");
+const express = require("express");
 
-const app = express()
+const app = express();
 
 app.use((req, res) => {
   //   res.send("Welcome to the express")
-  res.end("Welcome to the express")
-})
+  res.end("Welcome to the express");
+});
 
-const server = http.createServer(app)
-
-server.listen(3012, "127.0.0.1", () => {
-  console.log("server listening on port 3012")
-})
+app.listen(3012, "127.0.0.1", () => {
+  console.log("server listening on port 3012");
+});
